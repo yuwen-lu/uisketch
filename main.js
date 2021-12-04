9a5debb4f2380c3751633cfb11e8d7ae21d59359
Yuwen <yuwenlu@andrew.cmu.edu>
2021-12-04 13:31:29 -0500

    Make selection box work

--- main.js
+++ main.js
@@ -43,15 +43,23 @@ let computeSelected = (cursorStartX, cursorStartY, cursorEndX, cursorEndY) => {
 // draw a frame around selected box
 let drawSelectionBox = (selectedFrame) => {
   var div = document.createElement("div");
+  div.setAttribute("class", "selection-box");
   div.style.position = "absolute";
   div.style.top = selectedFrame.top + "px";
   div.style.left = selectedFrame.left + "px";
-  div.style.bottom = selectedFrame.bottom + "px";
-  div.style.right = selectedFrame.right + "px";
-  div.style.border = "5px solid red";
+  div.style.bottom = (window.innerHeight - selectedFrame.bottom) + "px";
+  div.style.right = (window.innerWidth - selectedFrame.right) + "px";
 
   console.log(div);
   document.querySelector('body').appendChild(div);
+
+  // clean up the variables
+  selectedFrame = {
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0
+  };
 }
 
 // Get cursor position, when drag starts
@@ -59,6 +67,8 @@ dragSelect.subscribe('dragstart', (e) => {
   // console.log(e);
   cursorStartX = dragSelect.getCurrentCursorPosition()['x'];
   cursorStartY = dragSelect.getCurrentCursorPosition()['y'];
+
+  console.log("cursorStartX: " + cursorStartX + ", cursorStartY: " + cursorStartY);
 })
 
 // Get cursor position, when drag ends
@@ -66,8 +76,12 @@ dragSelect.subscribe('callback', (e) => {
   // console.log(e);
   cursorEndX = dragSelect.getCurrentCursorPosition()['x'];
   cursorEndY = dragSelect.getCurrentCursorPosition()['y'];
-
+  console.log("cursorEndX: " + cursorEndX + ", cursorEndY: " + cursorEndY);
+  
+  // If the starting point is different from the end point,
   // compute the selected portion of the img
-  var selectedFrame = computeSelected(cursorStartX, cursorStartY, cursorEndX, cursorEndY);
-  drawSelectionBox(selectedFrame);
+  if (cursorStartX != cursorEndX || cursorStartY != cursorEndY) {
+    var selectedFrame = computeSelected(cursorStartX, cursorStartY, cursorEndX, cursorEndY);
+    drawSelectionBox(selectedFrame);
+  }
 })
